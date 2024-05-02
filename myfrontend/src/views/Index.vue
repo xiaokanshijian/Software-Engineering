9d1318f8816b737e109ba5c8f3d4485d77472eed
杨谦 <2692513479@qq.com>
2024-05-02 22:53:28 +0800

    修改首页布局

--- myfrontend/src/views/Index.vue
+++ myfrontend/src/views/Index.vue
@@ -1,157 +1,97 @@
 <template>
   <div class="home">
     <transition name="fade" mode="out-in">
-     
-      <dv-border-box-10
-        ><div class="naca">
+
+      <dv-border-box-10>
+        <div class="naca">
           <div class="index-header" style="margin-top: 5px">
             <div>
-              <dv-decoration-10
-                style="width: 450px; height: 1px; margin-bottom: 45px"
-              />
-              <dv-decoration-8
-                style="width: 180px; height: 50px"
-                :color="['#568aea', '#000000']"
-              />
-              <div
-                style="
+              <dv-decoration-10 style="width: 450px; height: 1px; margin-bottom: 45px" />
+              <dv-decoration-8 style="width: 180px; height: 50px" :color="['#568aea', '#000000']" />
+              <div style="
                   width: 150px;
                   color: #eeecec;
                   font-size: 18px;
                   padding: 0 15px;
                   font-weight: bold;
-                "
-              >
+                ">
                 可视化化平台
               </div>
-              <dv-decoration-8
-                :reverse="true"
-                style="width: 180px; height: 50px"
-                :color="['#568aea', '#000000']"
-              />
-              <dv-decoration-10
-                style="
+              <dv-decoration-8 :reverse="true" style="width: 180px; height: 50px" :color="['#568aea', '#000000']" />
+              <dv-decoration-10 style="
                   width: 450px;
                   height: 1px;
                   transform: rotateY(180deg);
                   margin-bottom: 45px;
-                "
-              />
+                " />
             </div>
-            <dv-decoration-5
-              style="width: 10%; height: 20px"
-              :color="['#568aea', '#000000']"
-            />
+            <dv-decoration-5 style="width: 10%; height: 20px" :color="['#568aea', '#000000']" />
           </div>
 
           <div class="index-content">
             <div class="left">
-              <div class="left-1" style="">
-               
-                <dv-border-box-12><div style="padding: 5px">
+              <div class="col-1" style="">
+
+                <dv-border-box-12 style="height: 300px; width: 490px">
                   <div class="title" style="margin-top: 5px">
-                    标题1
+                    历史记录
                   </div>
+                  <div ref="firstMain" style="width: 100%; height: 105%"></div>
+                </dv-border-box-12>
 
-                  <div ref="firstMain" style="width: 100%; height: 120px"></div>
-                </div></dv-border-box-12>
-                <dv-border-box-12><div style="padding: 5px;padding-bottom:30px">
-                  <div class="title" style="margin-top: 1px">标题1</div>
-                  <dv-capsule-chart
-                    
-                    style="width: 80%; height: 110px"
-                  />
-                </div></dv-border-box-12>
-                
-                <dv-border-box-12><div style="padding: 15px">
-                  <div class="title" style="margin-top: 5px">标题1</div>
-                  <!-- <div ref="timeZhou" style="width: 100%; height: 350px"></div> -->
-                  <div class="row_list" style="">
-                    <ul
-                      class="cases_list"
-                      style="width: 100%; height: 159px; overflow: auto"
-                    >
-                      
-                      </li>
-                    </ul>
+                <dv-border-box-12 style="height: 250px; width: 490px">
+                  <div class="title" style="margin-top: 5px">
+                    设备状态
                   </div>
-                </div></dv-border-box-12>
-               
+                  <div ref="secondMain" style="width: 100%; height: 90px"></div>
+                </dv-border-box-12>
 
-                
+
+
               </div>
             </div>
-            <div class="cents">
-              <div class="above">
-                <div class="aboveOne">
-                  <div style="padding: 15px">
-                    <div class="title">标题1</div>
-                    <div
-                      style="
-                        display: flex;
-                        flex-direction: column;
-                        width: 100%;
-                        height: 120px;
-                        color: #eeecec;
-                      "
-                    >
-                      <div style="display: flex; flex: 1">
-                        
 
-                        
-                      </div>
-                      <div style="display: flex; flex: 1">
-                       
-                      </div>
-                    </div>
-                  </div>
-                  <div style="padding: 15px">
-                    <div class="title" style="margin-top: -30px">
-                      标题1
-                    </div>
-                    <div class="content">
-                      
-                      <div style="width: 430px; height: 90px"></div>
-                    </div>
-                  </div>
-                </div>
-                <div class="aboveTwo">
-                  <dv-border-box-12 > <div style="padding: 15px">
+            <div class="cent">
+              <div class="col-2" style="">
+
+                <dv-border-box-12 style="height: 300px; width: 410px">
                   <div class="title" style="margin-top: 5px">
-                    标题1
+                    水文气象
                   </div>
-                  <div id="secondMian" style="width: 100%; height: 110px"></div>
-                </div></dv-border-box-12>
-                  <dv-border-box-12><div style="padding: 5px">
-                    <div class="title" style="margin-top: 5px">
-                      标题1
-                    </div>
-                    <div
-                      ref="thirdMain"
-                      style="width: 400px; height: 90px"
-                    ></div>
-                  </div></dv-border-box-12>
-                  
-                </div>
-              </div>
-              <div class="below">
-                
-                  <dv-border-box-12>
-                    <div style="padding: 7px">
-                      <div class="title" style="margin-top: 5px">
-                        标题1
-                      </div>
-                      <div
-                        ref="lastMain"
-                        style="width: 100%; height: 200px; margin-top: 25px"
-                      ></div>
-                    </div>
-                  </dv-border-box-12>
-                
+                  <div ref="thirdMain" style="width: 100%; height: 90px"></div>
+                </dv-border-box-12>
+
+                <dv-border-box-12 style="height: 250px; width: 410px">
+                  <div class="title" style="margin-top: 5px">
+                    定位
+                  </div>
+                  <div ref="fourthMain" style="width: 100%; height: 90px"></div>
+                </dv-border-box-12>
+
               </div>
             </div>
-          </div></div
-      ></dv-border-box-10>
+
+            <div class="right">
+              <div class="col-3" style="">
+                <dv-border-box-12 style="height: 330px; width: 540px">
+                  <div class="title" style="margin-top: 5px">
+                    监控视频
+                  </div>
+                  <div id="fifthMain" style="width: 500px; height: 90px"></div>
+                </dv-border-box-12>
+
+                <dv-border-box-12 style="height: 220px; width: 540px">
+                  <div class="title" style="margin-top: 5px">
+                    附加功能
+                  </div>
+                  <div id="sixthMain" style="width: 100%; height: 90px"></div>
+                </dv-border-box-12>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </dv-border-box-10>
     </transition>
   </div>
 </template>
@@ -159,6 +99,7 @@
 <script>
 import $ from "jquery";
 import LeftTop from "@/components/LeftTop.vue";
+import { color } from "echarts";
 function formatter(number) {
   const numbers = number.toString().split("").reverse();
   const segs = [];
@@ -171,10 +112,129 @@ export default {
   name: "Index",
   data() {
     return {
-    
+
     };
   },
+  methods: {
+    setBarData() {
+      $(document).ready(() => {
+        var chartDom = this.$refs.firstMain;
+        var myChart = this.$echarts.init(chartDom);
+        var option;
+        option = {
+          tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+              type: 'cross',
+              crossStyle: {
+                color: '#999'
+              }
+            }
+          },
+          toolbox: {
+            feature: {
+              dataView: { show: false, readOnly: false },
+              magicType: { show: true, type: ['line', 'bar'] },
+              restore: { show: false },
+              saveAsImage: { show: false }
+            }
+          },
+          legend: {
+            data: ['Evaporation', 'Precipitation', 'Temperature'],
+            textStyle:{
+              color: '#ffffff',
+            }
+          },
+          xAxis: [
+            {
+              type: 'category',
+              data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+              axisPointer: {
+                type: 'shadow'
+              },
+              show:false
+            }
+          ],
+          yAxis: [
+            {
+              type: 'value',
+              name: 'Precipitation',
+              nameTextStyle: {
+                color: '#ffffff',
+              },
+              min: 0,
+              max: 250,
+              interval: 50,
+              axisLabel: {
+                formatter: '{value} ml',
+                color: '#ffffff',
+                fontSize:10
+              }
+            },
+            {
+              type: 'value',
+              name: 'Temperature',
+              nameTextStyle: {
+                color: '#ffffff'
+              },
+              min: 0,
+              max: 25,
+              interval: 5,
+              axisLabel: {
+                formatter: '{value} °C',
+                color: '#ffffff',
+                fontSize:10
+              }
+            }
+          ],
+          series: [
+            {
+              name: 'Evaporation',
+              type: 'bar',
+              tooltip: {
+                valueFormatter: function (value) {
+                  return value + ' ml';
+                }
+              },
+              data: [
+                2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3
+              ]
+            },
+            {
+              name: 'Precipitation',
+              type: 'bar',
+              tooltip: {
+                valueFormatter: function (value) {
+                  return value + ' ml';
+                }
+              },
+              data: [
+                2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3
+              ]
+            },
+            {
+              name: 'Temperature',
+              type: 'line',
+              yAxisIndex: 1,
+              tooltip: {
+                valueFormatter: function (value) {
+                  return value + ' °C';
+                }
+              },
+              data: [2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2]
+            }
+          ]
+        };
 
+        option && myChart.setOption(option);
+      });
+    },
+  },
+  async created() {
+    $(document).ready(async () => {
+      this.setBarData()
+    })
+  },
   components: {
     LeftTop,
   },
@@ -188,48 +248,58 @@ export default {
   top: 50%;
   transform: translate(-50%, -50%);
 }
+
 .cent-1-content {
   padding: 20px;
   display: flex;
 }
+
 .right-content {
   margin-left: 30px;
   display: flex;
   flex-direction: column;
   justify-content: space-around;
 }
+
 .right-content div {
   display: flex;
   font-size: 15px;
   align-items: center;
 }
+
 .fade-enter-active,
 .fade-leave-active {
   transition: opacity 0.5s;
 }
+
 .fade-enter,
 .fade-leave-to {
   opacity: 0;
 }
-.cents {
-  display: flex;
-  flex-direction: column;
-}
+
+// .cents {
+//   display: flex;
+//   flex-direction: column;
+// }
+
 .above {
   display: flex;
 }
+
 .aboveOne {
   display: flex;
   flex-direction: column;
 }
+
 .aboveTwo {
   display: flex;
   flex-direction: column;
 }
-.cent {
-  width: 850px;
-  height: 300px;
-}
+
+// .cent {
+//   width: 850px;
+//   height: 300px;
+// }
 
 .cent-1 {
   margin: 10px;
@@ -244,17 +314,53 @@ export default {
   flex-direction: column;
 }
 
+.cent {
+  display: flex;
+  flex-direction: column;
+}
+
+.right {
+  display: flex;
+  flex-direction: column;
+}
+
 .left-1 {
-  margin: 15px;
+  margin: 5px;
   color: aliceblue;
   width: 550px;
   display: flex;
   flex-direction: column;
 }
+
 .left-2 {
-  margin: 15px;
+  margin: 5px;
   color: aliceblue;
   width: 530px;
+  display: flex;
+  flex-direction: column;
+}
+
+.col-1 {
+  margin-right: 30px;
+  color: aliceblue;
+  width: 430px;
+  display: flex;
+  flex-direction: column;
+}
+
+.col-2 {
+  margin-left: 30px;
+  margin-right: 55px;
+  color: aliceblue;
+  width: 300px;
+  display: flex;
+  flex-direction: column;
+}
+
+.col-3 {
+  margin-left: 55px;
+  color: aliceblue;
+  width: 540px;
   display: flex;
   flex-direction: column;
 }
@@ -267,42 +373,48 @@ export default {
   display: flex;
   flex-direction: column;
 }
+
 .naca .index-header {
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
 }
+
 .naca .index-header div {
   display: flex;
   justify-content: center;
   align-items: center;
 }
+
 .naca .index-content {
   display: flex;
   justify-content: center;
   align-items: center;
 }
+
 .bg {
   width: 100%;
   height: 45rem;
   background-color: black;
   position: relative;
 }
+
 .title {
   color: #3f96a5;
   font-size: 18px;
-  margin-top: -20px;
   text-align: center;
   display: flex;
   flex-direction: column;
   align-items: center;
   font-weight: bold;
 }
+
 .content {
   display: flex;
   align-items: center;
 }
+
 .content-word {
   width: 140px;
   height: 130px;
@@ -313,26 +425,32 @@ export default {
   flex-direction: column;
   justify-content: center;
 }
+
 .content-word-item {
   margin-left: 19px;
   margin-bottom: 10px;
+
   img {
     width: 20px;
     height: 20px;
   }
 }
+
 .content-word-item-title {
   font-size: 18px;
 }
+
 .content-word-item-content {
   margin-top: 5px;
 
   display: flex;
   align-items: center;
 }
+
 .row_list {
   list-style: none;
 }
+
 .cases_list::-webkit-scrollbar {
   display: none;
 }
@@ -347,10 +465,12 @@ export default {
   line-height: 30px;
   color: rgb(238, 236, 236);
 }
+
 .list_time {
   height: 30px;
   overflow: auto;
 }
+
 .list_time::-webkit-scrollbar {
   display: none;
 }
